7142f461e0893a2da0bd1ee0b7f680b60519bf1b
Marc4k <marcjenni100@gmail.com>
2022-05-31 13:22:37 +0200

    added Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:chat_app/pages/create_profil_screen/view/create_profil_screen.dart';
 import 'package:chat_app/pages/welcome_screen/view/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
